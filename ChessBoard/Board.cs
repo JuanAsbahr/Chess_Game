e66258b4fd6969b6c95f6e06b12143b582a60330
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-04 15:41:05 -0300

    ChessBoard - Valid Position

--- ChessBoard/Board.cs
+++ ChessBoard/Board.cs
@@ -18,10 +18,38 @@
             return pieces[line, column];
         }
 
+        public Piece piece (Position position)
+        {
+            return pieces[position.Line, position.Column];
+        }
+
+        public bool thereIsPiece(Position position)
+        {
+            validatePosition(position);
+            return piece(position) != null;
+        }
+
         public void insertPiece (Piece piece, Position position)
         {
             pieces[position.Line, position.Column] = piece;
             piece.position = position;
+        }
+
+        public bool validPosition(Position position)
+        {
+            if (position.Line<0 || position.Line>=lines || position.Column <0 || position.Column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validatePosition(Position position)
+        {
+            if (!validPosition(position))
+            {
+                throw new BoardException("Invalid Position");
+            }
         }
     }
 }
