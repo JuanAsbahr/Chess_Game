192281fa08cd5b4a11a1d8e0103c8d1970d2af8e
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-04 10:54:31 -0300

    ChessBoard - Position

---
+++ ChessBoard/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess_Game.ChessBoard
+{
+    internal class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position(int line, int column)
+        {
+            this.Line = line;
+            this.Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line + " " + Column;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess_Game.ChessBoard;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Position P;
+
+        P = new Position(3, 4);
+        Console.WriteLine("Position: " + P);
+    }
+}
