645602ea0d286f99dff441f2ad53cce9ee09f599
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-05 16:06:51 -0300

    Pawn's Move

--- Chess/Pawn.cs
+++ Chess/Pawn.cs
@@ -12,5 +12,70 @@ namespace Chess_Game.Chess
         {
             return "P";
         }
+
+        private bool opponent(Position position)
+        {
+            Piece piece = board.piece(position);
+            return piece != null && piece.color != color;
+        }
+
+        private bool empty(Position position)
+        {
+            return board.piece(position) == null;
+        }
+        public override bool[,] possibleMoves()
+        {
+            bool[,] matrix = new bool[board.lines, board.columns];
+
+            Position position = new Position(0, 0);
+
+            if (color == Color.White)
+            {
+                position.setValues(position.Line - 1, position.Column);
+                if (board.validPosition(position) && empty(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line - 2, position.Column);
+                if (board.validPosition(position) && empty(position) && moves == 0)
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line - 1, position.Column - 1);
+                if (board.validPosition(position) && opponent(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line - 1, position.Column + 1);
+                if (board.validPosition(position) && opponent(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+            }
+            else
+            {
+                position.setValues(position.Line + 1, position.Column);
+                if (board.validPosition(position) && empty(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line + 2, position.Column);
+                if (board.validPosition(position) && empty(position) && moves == 0)
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line + 1, position.Column - 1);
+                if (board.validPosition(position) && opponent(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+                position.setValues(position.Line + 1, position.Column + 1);
+                if (board.validPosition(position) && opponent(position))
+                {
+                    matrix[position.Line, position.Column] = true;
+                }
+            }
+            return matrix;
+        }
     }
 }
