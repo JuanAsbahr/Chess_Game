0d7ca19671b6449a7ef70ece5a25aea0d8cae3e6
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-05 15:47:55 -0300

    Horse's Move

--- Chess/Horse.cs
+++ Chess/Horse.cs
@@ -12,5 +12,67 @@ namespace Chess_Game.Chess
         {
             return "H";
         }
+
+        private bool canMove(Position position)
+        {
+            Piece piece = board.piece(position);
+            return piece == null || piece.color != color;
+        }
+        public override bool[,] possibleMoves()
+        {
+            bool[,] matrix = new bool[board.lines, board.columns];
+
+            Position position = new Position(0, 0);
+
+            //North
+            position.setValues(position.Line - 1, position.Column -2);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //North East
+            position.setValues(position.Line - 2, position.Column  - 1);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //East
+            position.setValues(position.Line - 2, position.Column + 1);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //Southeast
+            position.setValues(position.Line - 1, position.Column + 2);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //South
+            position.setValues(position.Line + 1, position.Column + 2);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //South-West
+            position.setValues(position.Line + 2, position.Column + 1);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //West
+            position.setValues(position.Line + 2, position.Column - 1);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            //Northwest
+            position.setValues(position.Line + 1, position.Column - 2);
+            if (board.validPosition(position) && canMove(position))
+            {
+                matrix[position.Line, position.Column] = true;
+            }
+            return matrix;
+        }
     }
 }
