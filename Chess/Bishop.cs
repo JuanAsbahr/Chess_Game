e0c82b4f98acba864fe4068d563e5d79108ef7e4
JuanAsbahr <juan_asbahr@hotmail.com>
2023-07-10 10:29:43 -0300

    Bishop

---
+++ Chess/Bishop.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Chess_Game.ChessBoard;
+
+namespace Chess_Game.Chess
+{
+    internal class Bishop : Piece
+    {
+        public Bishop(Board board, Color color) : base(color, board)
+        {
+        }
+        public override string ToString()
+        {
+            return "B";
+        }
+
+        private bool canMove(Position pos)
+        {
+            Piece p = board.piece(pos);
+            return p == null || p.color != color;
+        }
+        public override bool[,] possibleMoves()
+        {
+            bool[,] matrix = new bool[board.lines, board.columns];
+
+            Position pos = new Position(0, 0);
+
+            //Northwest
+            pos.setValues(position.line - 1, position.column - 1);
+            while (board.validPosition(pos) && canMove(pos))
+            {
+                matrix[pos.line, pos.column] = true;
+                if (board.piece(pos) != null && board.piece(pos).color != color)
+                {
+                    break;
+                }
+                pos.setValues(pos.line - 1, pos.column - 1);
+            }
+            //North East 
+            pos.setValues(position.line - 1, position.column + 1);
+            while (board.validPosition(pos) && canMove(pos))
+            {
+                matrix[pos.line, pos.column] = true;
+                if (board.piece(pos) != null && board.piece(pos).color != color)
+                {
+                    break;
+                }
+                pos.setValues(pos.line - 1, pos.column + 1);
+            }
+            //Southeast
+            pos.setValues(position.line + 1, position.column + 1);
+            while (board.validPosition(pos) && canMove(pos))
+            {
+                matrix[pos.line, pos.column] = true;
+                if (board.piece(pos) != null && board.piece(pos).color != color)
+                {
+                    break;
+                }
+                pos.setValues(pos.line + 1, pos.column + 1);
+            }
+            //South-West
+            pos.setValues(position.line + 1, position.column - 1);
+            while (board.validPosition(pos) && canMove(pos))
+            {
+                matrix[pos.line, pos.column] = true;
+                if (board.piece(pos) != null && board.piece(pos).color != color)
+                {
+                    break;
+                }
+                pos.setValues(pos.line + 1, pos.column - 1);
+            }
+
+            return matrix;
+        }
+    }
+}
